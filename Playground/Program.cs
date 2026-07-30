a6a3075189f96fb638201cd63cfa8ad92866cf21
Aarya <krishnamantradhidevata@gmail.com>
2026-07-30 23:14:26 +0530

    Day - 7 Count Digits

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,36 +1,22 @@
-﻿// Reverse an array {two pointers}
-// Given {2, 8, 5, 9, 1}
-// o/p {1,9,5,8,2 }
+﻿// Count Digits
+//given 12345
+// o/p 5
 
-int[] a = new int[] { 2, 8, 5, 9, 1 };
-int left = 0;
-int right = a.Length - 1;
-for (int i=0; i<a.Length/2; i++)
+// Rules:
+// ❌ Don't convert the number to a string.
+// ❌ Don't use .Length.
+// ✅ Use arithmetic (/ and %).
+
+//idea:extracting digits using mathematics,
+//  which you'll use later for reversing numbers, 
+// palindrome numbers, Armstrong numbers, and more.
+
+using System;
+
+int a = 12345;
+int count = 0;
+ for(; a != 0; a = a/10)
 {
-   // i do know that we have to do something with 
-   // a[length-i] = a[i]
-   // that means storing values, but i want left 
-   // to be pointing at index 0 not storing the value of index 0
-   // now we'll swap
-   int temp = a[left];
-   a[left] = a[right];
-   a[right] = temp;
-    //after every swap, what should happen?
-   //Left should move one step forward.
-   //Right should move one step backward.
-   left++;
-   right--;
+    count++;
 }
-
-for (int i = 0; i < a.Length; i++)
-{
-    Console.Write(a[i] + " ");
-}
-
-//another possible solution
-// while (left < right)
-// {
-//     ...
-//     left++;
-//     right--;
-// }
+Console.WriteLine("Number of digits = " + count);
